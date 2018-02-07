afd49c411669beaf6660605f95be3adbff76ef7e
Ryan Ferner <ryan.ferner@duke.edu>
2018-02-07 17:55:50 -0500

    i tried my darndest, but to no avail

--- js/greenscreen.js
+++ js/greenscreen.js
@@ -1,15 +1,53 @@
 /*
  * Add code here to create a composite of two images by replacing the green pixels in the foreground image.
  *
- * Author: YOUR NAME HERE
+ * Author: Ryan Ferner
  */
 // useful function to clear the contents of a given canvas
+var foregroundImage = null;
+var backgroundImage = null;
+var compositeImage = null;
+var fgcanvas = document.getElementById('fgcanvas');
+var bgcanvas = document.getElementById('bgcanvas');
+var canvas3 = document.getElementById('canvas3');
+
+
+document.getElementById('bgInput').addEventListener('change', load1stImage, false);
+document.getElementById('fgInput').addEventListener('change', load2ndImage, false);
+document.getElementById('threshold').addEventListener('input', greenscreen, false);
+document.getElementById('create').addEventListener('change', greenscreen, false);
+document.getElementById('clear').addEventListener('change', clearCanvas, false);
+
+
+function load1stImage() {
+    var file = document.getElementById('bgInput');
+    foregroundImage = new SimpleImage(file);
+    foregroundImage.setSize(bgcanvas.width, bgcanvas.height);
+    console.log("mein gott");
+    foregroundImage.drawTo(fgcanvas);
+}
+
+function load2ndImage() {
+    var file = document.getElementById('fgInput');
+    backgroundImage = new SimpleImage(file);
+    backgroundImage.setSize(bgcanvas.width, bgcanvas.height);
+    backgroundImage.drawTo(bgcanvas);
+}
+
 function clearCanvas(canvas) {
     var context = canvas.getContext("2d");
     context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function greenScreen(threshold) {
+function greenscreen(threshold) {
+        if (foregroundImage == null) {
+        alert('Original image not loaded');
+    }
+        if (backgroundImage == null) {
+        alert('background image not loaded');
+    }
+    threshold = document.getElementById('threshold').value;
+    deGreening(threshold);
     // given foreground and background images, make a new image of the canvas size
 
     // For each pixel in foreground image
@@ -23,4 +61,12 @@ function greenScreen(threshold) {
             // set output image's corresponding pixel to current pixel
 
     // return resulting image
+    
+    compositeImage.drawTo(fgcanvas);
+}
+
+function deGreening(threshold){
+    compositeImage = new SimpleImage(file);
+
+    return compositeImage;
 }
